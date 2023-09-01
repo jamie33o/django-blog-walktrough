59e36aa8ac9c5df0826664aeda213fecd0dff67c
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-09-01 15:12:49 +0100

    Fix: wrong syntax  allowed hosts and media_url

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://mega-blog.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['mega-blog-df9f0010e30a.herokuapp.com', 'localhost']
 
 
 # Application definition
