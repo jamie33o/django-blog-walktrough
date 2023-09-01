6985660df05942bb61816dd48351f28860594940
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-09-01 16:36:33 +0100

    added comments and posts to admin panel with summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Post,Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)} # make the url of post match the title in slug format 
+    list_filter = ('status', 'created_on')# creates a section in admin to filter content based on created on 
+    summernote_fields = ('content')# summer not gives more design option on input fields when entering text in content field admin panel
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name','body','post','approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField# for featured image
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))# this is tuble for the status
+
+
+class Post(models.Model):# for the posts table in the database
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)# slug field is a description containing only letters, hyphens, numbers or underscores.It is often used in url's to make them easier to read, but also to make them more search engine friendly.
+    author = models.ForeignKey(# foreign key relationship one to many relation
+        User, on_delete=models.CASCADE, related_name="blog_posts"# cascade deletes any feilds related to this field
+    )
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
+
+    class Meta:
+        ordering = ["-created_on"] #
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):# comments model for database
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
