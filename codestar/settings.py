ac770c9c1c2b178c421d05b8ba81ac8aba941f0b
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-09-01 22:15:33 +0100

    finished walktrough

--- codestar/settings.py
+++ codestar/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.sites',
     'allauth',
+    'bootstrap4,'
     'allauth.account',
     'allauth.socialaccount',
     'cloudinary_storage',
