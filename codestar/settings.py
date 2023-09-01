ee9871166ad49486a87f728e5fc3b2fd437a6412
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-09-01 13:33:36 +0100

    Fix: wrong allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mega-blog.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['https://mega-blog-df9f0010e30a.herokuapp.com/', 'localhost']
 
 
 # Application definition
